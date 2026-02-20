94c099c21aee85839a9edbd9ce30f6344093e573
sharadhi-kodagi <sharadhishalu@gmail.com>
2026-02-20 17:17:02 +0530

    Update settings.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -109,6 +109,11 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# --- Resend Email Configuration ---
+RESEND_API_KEY = os.environ.get("RESEND_API_KEY")
+DEFAULT_FROM_EMAIL = "redhatcat727@gmail.com" 
+
+
 
 
 
